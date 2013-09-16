28364ff35c26c253a12924ef806ea469d0d2860a
ISTweak <istweak@d-o-b.org>
2013-09-17 06:21:43 +0900

    update
    Change-Id: Ib35c78d4db4ab2fc3294f7ff1078ea1b47a427ef

--- extract-files.sh
+++ extract-files.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-# This script is maintained in the common tree
-../msm8660-common/extract-files.sh $*
+BASE=../../../vendor/samsung/quincydcm/proprietary
+rm -rf $BASE/system
 
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/system/$DIR ]; then
+    mkdir -p $BASE/system/$DIR
+  fi
+  adb pull /system/$FILE $BASE/system/$FILE
+done
+
+adb pull /system/lib/hw/sensors.MSM8660_SURF.so $BASE/system/lib/hw/sensors.msm8660.so
+
+./setup-makefiles.sh
