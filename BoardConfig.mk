1f1b984da9de343e3c74ec35e9aa22fbfe9c51d7
Marijuana <istweak@d-o-b.org>
2015-02-14 10:35:26 +0900

    update cm-12.0
    Change-Id: Ia4188862f6df530078d0eb9066438d959726e369

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,8 +20,9 @@
 # definition file).
 #
 
-# inherit from common msm8660
--include device/samsung/msm8660-common/BoardConfigCommon.mk
+# inherit from common quincy
+TWRP := true
+-include device/samsung/quincy-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/quincydcm/BoardConfigVendor.mk
@@ -32,47 +33,24 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 TARGET_OTA_ASSERT_DEVICE := SC-05D,quincydcm
 
 # Kernel
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
-BOARD_KERNEL_BASE           := 0x48000000
-BOARD_KERNEL_PAGESIZE       := 2048
-BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01600000
-TARGET_KERNEL_CONFIG        := cyanogenmod_quincydcm_defconfig
-TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7-sm
-TARGET_GCC_VERSION := 4.8-sm
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache androidboot.selinux=permissive
+TARGET_KERNEL_CONFIG := cyanogenmod_quincyatt_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8660
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin
 
-# Assert minimum baseband version
-TARGET_BOARD_INFO_FILE ?= device/samsung/quincydcm/board-info.txt
-
+# Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1570766848
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2149580800
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/quincydcm/bluetooth
 
-# Suppress the WIPE command since it can brick our EMMC
-BOARD_SUPPRESS_EMMC_WIPE := true
-
-# Disable initlogo, Samsungs framebuffer is weird
-TARGET_NO_INITLOGO := true
-
-# Preload the boot animation to avoid jerkiness
-TARGET_BOOTANIMATION_PRELOAD := true
-
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p28
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_RECOVERY_SWIPE := true
-
 # Recovery
 BOARD_RECOVERY_SWIPE := true
-BOARD_HAS_NO_SELECT_BUTTON := true
 
 # secondary sdcard
 BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p29
@@ -84,14 +62,12 @@ BOARD_RECOVERY_SWIPE := true
 DEVICE_RESOLUTION := 800x1280
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
-TW_INTERNAL_STORAGE_PATH := "/sdcard"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TARGET_USERIMAGES_USE_F2FS := true
+
 TW_FLASH_FROM_STORAGE := true
-TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
 TW_NO_EXFAT := true
-SP1_NAME := "efs"
-SP1_BACKUP_METHOD := files
-SP1_MOUNTABLE := 1
+TW_NO_USB_STORAGE := true
+TW_NO_EXFAT := true
+TW_NO_SCREEN_TIMEOUT := true
--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/quincydcm/quincydcm-vendor.mk)
 
@@ -25,57 +23,25 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/quincydcm/overlay
 # Ramdisk
 PRODUCT_PACKAGES += \
     initlogo.rle \
-    init.target.rc \
     init.qcom.usb.rc
-
-# quincydcm Ramdisk
-PRODUCT_COPY_FILES += \
-	device/samsung/quincydcm/ramdisk/fstab.qcom:root/fstab.qcom
 
 # BT firmware
 PRODUCT_COPY_FILES += \
     device/samsung/quincydcm/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
 
-# Japanese font
-PRODUCT_COPY_FILES += \
-    device/samsung/quincydcm/fallback_fonts.xml:system/vendor/etc/fallback_fonts.xml \
-	frameworks/base/data/fonts/DroidSansJapanese.ttf:system/fonts/DroidSansJapanese.ttf
+# FM radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    FMRecord \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # Change the default locale to Japanese.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.locale.language=ja \
     ro.product.locale.region=JP
 
-## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Hardware
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml 
-
-# S-Pen IDC
-PRODUCT_COPY_FILES += \
-    device/samsung/quincydcm/idc/sec_e-pen.idc:system/usr/idc/sec_e-pen.idc \
-    device/samsung/quincydcm/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc    
-
-# QRNGD
-PRODUCT_PACKAGES += qrngd
-
-# Bootanimation
-TARGET_SCREEN_WIDTH := 800
-TARGET_SCREEN_HEIGHT := 1280
-
-# Common features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
-
-# common msm8660
-$(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
-
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+# Inherit from quincy-common
+$(call inherit-product, device/samsung/quincy-common/quincy-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/quincydcm/quincydcm-vendor.mk)
